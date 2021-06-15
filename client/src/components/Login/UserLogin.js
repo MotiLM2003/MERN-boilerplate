e5506ac74636a97b5dad16b0fa3894c976672850
Moti Elmakies <motiphone2003@gmail.com>
2021-06-15 16:57:12 +0300

    final changes

--- client/src/components/Login/UserLogin.js
+++ client/src/components/Login/UserLogin.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import logo from '../../images/logo-icon.png';
+
 const sdd = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        height: ' 100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <img src={logo} alt='Company logo' />
+    </div>
+  );
 };
 
 export default sdd;
